250f7eaf5b9f482c7a868d35f24ed654f8c30a28
Andrzej Fudro <s416065@labs.wmi.amu.edu.pl>
2017-10-24 13:01:58 +0200

    exercise 3

---
+++ Lesson_01/exercise_131.js
@@ -0,0 +1,35 @@
+function Figura(nazwa){
+  this.nazwa = nazwa;
+}
+
+function Czworokat(nazwa, typ_czworokata,a,b,c,d) {
+        Figura.call(this, nazwa);
+        this.typ_czworokata = typ_czworokata;
+		this.a=a;
+		this.b=b;
+		this.c=c;
+		this.d=d;
+}
+
+Czworokat.prototype = Object.create(Figura.prototype);
+Czworokat.prototype.constructor = Czworokat;
+
+function Prostokat(nazwa, typ_czworokata,a,b,c,d) {
+        Czworokat.call(this, nazwa, typ_czworokata,a,b,c,d);
+}
+
+Prostokat.prototype.podajObwod = function (){
+		return (a+b+c+d);
+}
+Prostokat.prototype.podajPole = function (){
+		return (a*b);
+}
+
+Prostokat.prototype = Object.create(Czworokat.prototype);
+Prostokat.prototype.constructor = Prostokat;
+
+x=new Prostokat("?!!!","prostokąt",5,8,5,8);
+console.log(x.nazwa);
+console.log(x.typ_prostokata);
+console.log(x.podajPole());
+console.log(x.podajObwod());
